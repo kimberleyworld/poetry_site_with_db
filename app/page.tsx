8ab5d3f2e09dd3e3fc5249a9fbc7f8bdd90cd0d4
kimberleyworld <83449648+kimberleyworld@users.noreply.github.com>
2025-10-08 16:49:44 +0100

    update nov event

--- app/page.tsx
+++ app/page.tsx
@@ -14,15 +14,15 @@ export default function Home() {
       <WebGLBackground />
       <Navbar />
       <NextEventBanner 
-        eventDate="Tuesday 7th October"
+        eventDate="Tuesday 4th November"
         eventTime="19:00"
-        ticketUrl="https://www.headfirstbristol.co.uk/whats-on/bridge-farm/tue-7-oct-soft-spot-138665#e138665"/>
+        ticketUrl="https://hdfst.uk/e139475"/>
       <HeroSection />
       <ArchiveSection />
       <EventCard 
-        eventDate="Tuesday 7th October"
+        eventDate="Tuesday 4th November"
         eventTime="19:00"
-        ticketUrl="https://www.headfirstbristol.co.uk/whats-on/bridge-farm/tue-7-oct-soft-spot-138665#e138665"
+        ticketUrl="https://hdfst.uk/e139475"
       />
       <Footer />
     </div>
