36627c067835dd6b0c6d2f760220335e25a3e933
kimberleyworld <83449648+kimberleyworld@users.noreply.github.com>
2025-12-09 14:48:56 +0000

    jan event update

--- app/page.tsx
+++ app/page.tsx
@@ -14,15 +14,15 @@ export default function Home() {
       <WebGLBackground />
       <Navbar />
       <NextEventBanner 
-        eventDate="Tuesday 2nd December"
+        eventDate="Tuesday 6th Jan 2026"
         eventTime="19:00"
-        ticketUrl="https://www.headfirstbristol.co.uk/whats-on/bridge-farm/tue-2-dec-soft-spot-139477#e139477"/>
+        ticketUrl="https://www.headfirstbristol.co.uk/whats-on/bridge-farm/tue-6-jan-soft-spot-139478#e139478"/>
       <HeroSection />
       <ArchiveSection />
       <EventCard 
-        eventDate="Tuesday 2nd December"
+        eventDate="Tuesday 6th Jan 2026"
         eventTime="19:00"
-        ticketUrl="https://www.headfirstbristol.co.uk/whats-on/bridge-farm/tue-2-dec-soft-spot-139477#e139477"
+        ticketUrl="https://www.headfirstbristol.co.uk/whats-on/bridge-farm/tue-6-jan-soft-spot-139478#e139478"
       />
       <Footer />
     </div>
